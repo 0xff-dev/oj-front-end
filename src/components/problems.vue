e008fb916b602764251bb3869bcf41644bfcc8b0
shuang zhang <stevenshuang521@gmail.com>
2019-09-08 18:48:37 +0800

    problems list demo

---
+++ src/components/problems.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="problemContainer">
+        <TableHead 
+            title="Problmes List"
+            :refresh=true
+            optionsName="Tags" 
+            cpType="search"></TableHead>
+        <p>dappp</p>
+    </div>
+</template>
+
+<script>
+import TableHead from './tableHead.vue'
+
+export default {
+    name: 'Problems',
+    components: {
+        TableHead
+    }
+}
+</script>
+
+<style scoped>
+.problemContainer {
+    background-color: white;
+    box-shadow: 3px 3px gray;
+    height: 200px;
+}
+</style>
