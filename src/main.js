ef372954b11a551738050ececb3c5716f51ff067
shuang zhang <stevenshuang521@gmail.com>
2019-09-08 18:47:31 +0800

    change global css and add router entrance

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Problems from './components/problems.vue'
 import './assets/styles/global.css'
+import VueRouter from 'vue-router';
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+
+// 抽象出路由，不能写在这里
+const routes = [
+  {path: '/problems', component: Problems}
+]
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
