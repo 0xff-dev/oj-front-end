5050606e6e9f3047ca3ef304f017e1173eba6dd1
shuang zhang <stevenshuang521@gmail.com>
2019-09-14 23:42:43 +0800

    use paginations and add test datas

--- src/components/problems.vue
+++ src/components/problems.vue
@@ -15,16 +15,16 @@
                 <span>AcRate</span>
                 <span>Tags</span>
             </div>
-            <div v-for="(item,index) in problems" :key="item.ID" :class="index%2==0?'header white-back':'header'" >
-                <span><img :src="item.IsAc?rightPng:wrongPng" class="ac-img"></span>
-                <span>{{item.ID}}</span>
-                <span>{{item.Title}}</span>
-                <span>{{item.CommitCnt}}</span>
-                <span>{{item.AcRate}}</span>
-                <span>{{item.Tags}}</span>
+            <div v-for="(item,index) in datas" :key="item.id" :class="index%2==0?'header white-back':'header'" >
+                <span><img :src="item.is_ac?rightPng:wrongPng" class="ac-img"></span>
+                <span>{{item.id}}</span>
+                <span>{{item.title}}</span>
+                <span>{{item.commit_cnt}}</span>
+                <span>{{item.ac_rate}}</span>
+                <span>{{item.tags}}</span>
             </div>
         </div>
-        <Pagination :total=10></Pagination>
+        <Pagination :apiUrl="constUrl" :datas.sync="datas"></Pagination>
     </div>
 </template>
 
@@ -39,25 +39,10 @@ export default {
     },
     data: function() {
         return {
-            problems: [
-                {"ID":"1", "Title": "A+B", "CommitCnt": 100, "AcRate":"90%", "Tags":"a+b,h", "IsAc":true},
-                {"ID":"2", "Title": "A+B1", "CommitCnt": 100, "AcRate":"80%", "Tags":"a+b1,h","IsAc":true},
-                {"ID":"3", "Title": "A+B2", "CommitCnt": 100, "AcRate":"70%", "Tags":"a+b2,h","IsAc":true},
-                {"ID":"4", "Title": "A+B3dshdjshdsjdhsjd", "CommitCnt": 100, "AcRate":"60%", "Tags":"a+b3,h","IsAc":true},
-                {"ID":"5", "Title": "A+B4", "CommitCnt": 100, "AcRate":"50%", "Tags":"a+b4,h","IsAc":true},
-                {"ID":"6", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":false},
-                {"ID":"7", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":false},
-                {"ID":"8", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":false},
-                {"ID":"9", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":true},
-                {"ID":"10", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":false},
-                {"ID":"11111", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":false},
-                {"ID":"12", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":false},
-                {"ID":"13", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h,dskmdsd,d","IsAc":true},
-                {"ID":"14", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":false},
-                {"ID":"15", "Title": "A+B5", "CommitCnt": 100, "AcRate":"0%", "Tags":"a+b5,h","IsAc":false},
-            ],
+            datas: [],
             rightPng: require(`../assets/images/ac.png`),
-            wrongPng: require(`../assets/images/wrong.png`)
+            wrongPng: require(`../assets/images/wrong.png`),
+            constUrl: "http://localhost:8888/problems"
         }
     }
 }
@@ -66,8 +51,8 @@ export default {
 <style scoped>
 .problemContainer {
     background-color: white;
-    /* box-shadow: 3px 3px gray; */
-    height: 200px;
+    box-shadow: 0px 0px 5px gray;
+    height: auto;
 }
 
 .header {
