39dc2553bb357e4f39a90912bb3b2b5bd5c84df2
shuang zhang <stevenshuang521@gmail.com>
2019-09-14 23:41:09 +0800

    spell error and fix bugs

--- src/components/pagination.vue
+++ src/components/pagination.vue
@@ -9,43 +9,61 @@
             </span>
         </div>
         <div v-if="total >= 5">
-            <span class="defautl-span span-border" v-if="nowIndex!=1" @click="prePage"><img :src="left"></span>
+            <span class="default-span span-border" v-if="nowIndex!=1" @click="prePage"><img :src="left"></span>
             <span v-for="index in pages" 
                 :key="index" 
                 :class="index==nowIndex?'default-span span-border-color activePage':'default-span span-border-color '"
                 @click="selectPage(index)">
                     {{index}}
             </span>
-            <span class="defautl-span span-border" v-if="nowIndex!=total" @click="nextPage"><img :src="right"></span>
+            <span class="default-span span-border" v-if="nowIndex!=total" @click="nextPage"><img :src="right"></span>
         </div>
     </div> 
 </template>
 
 <script>
+import Axios from 'axios';
 
 export default {
     props: {
-        total: {
-            type: Number,
+        apiUrl: {
+            type: String,
+            required: true
+        },
+        datas: {
+            type: Array,
             required: true
         }
     },
     data: function() {
         return {
-            nowIndex: 1, 
+            nowIndex: 1,
+            total: 0,
             left: require(`../assets/images/left.png`),
             right: require(`../assets/images/right.png`),
             pages: [1,2,3,4,5]
         }
     },
+    created: function() {
+        this.selectPage(1)
+    },
     methods: {
         selectPage: function(index) {
             const that = this;
             that.nowIndex = index
-            that.pages = Array.from(function* gen(start, end){
-                while(start<=end) yield start++;
-            }(that.nowIndex==that.total||that.nowIndex==that.total-1||that.nowIndex==that.total-2?that.total-4:that.nowIndex-2>0?that.nowIndex-2:1, 
-                that.nowIndex==1||that.nowIndex==2||that.nowIndex==3?5:that.nowIndex+2<=that.total?that.nowIndex+2:that.total))
+            Axios.get(that.apiUrl).then(response => {
+                that.$emit('update:datas', response.data.data)
+                that.total = response.data.count
+                if(that.total >= 5) {
+                    that.pages = Array.from(function* gen(start, end){
+                        while(start<=end) yield start++;
+                    }(that.nowIndex >= that.total-2?that.total-4:that.nowIndex-2>0?that.nowIndex-2:1, 
+                        that.nowIndex <= 3?5:that.nowIndex+2<=that.total?that.nowIndex+2:that.total))
+                }
+                if(that.nowIndex > that.total) {
+                    that.nowIndex = that.total
+                }
+            })
         },
         prePage: function() {
             this.nowIndex--
@@ -83,10 +101,12 @@ export default {
     background-color: white;
     margin-left: 5px;
     cursor: pointer;
+    user-select: none;
 }
 
 .span-border {
     border-radius: 50%;
+    margin: 0 5px;
 }
 .span-border-color {
     border-radius: 5px;
