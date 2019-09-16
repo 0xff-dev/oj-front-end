78de714066c5dfa9b317fd3bbdf665a569729cb6
shuang zhang <stevenshuang521@gmail.com>
2019-09-16 23:50:00 +0800

    adjust code

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Problems from './components/problems.vue'
 import './assets/styles/global.css'
-import VueRouter from 'vue-router';
+import router from './components/router'
 
-Vue.use(VueRouter)
 Vue.config.productionTip = false
-
-// 抽象出路由，不能写在这里
-const routes = [
-  {path: '/problems', component: Problems}
-]
-const router = new VueRouter({
-  routes
-})
 
 new Vue({
   render: h => h(App),
