18ab4b9384ec6ee87af224e7dd40a04871718514
shuang zhang <stevenshuang521@gmail.com>
2019-09-20 23:30:15 +0800

    add comment components

--- src/components/views/home.vue
+++ src/components/views/home.vue
@@ -1,10 +1,25 @@
 <template>
-    <h1>Online Judge</h1>
+    <div>
+        <h1>Online Judge</h1>
+        <Comment :comments.sync="datas"></Comment>
+    </div>
 </template>
 
 <script>
+import Comment from '../comment.vue'
+
 export default {
-    
+    components: {
+        Comment,
+    },
+    data() {
+        return {
+            datas: []
+        }
+    },
+    // created: function() {
+    //     Axios.get("http://localhost:8888/comments").then(response => {this.datas = response.data.data})
+    // }
 }
 </script>
 
