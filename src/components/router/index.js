54af5b8879095ce166e63b533897fadde7b351f4
shuang zhang <stevenshuang521@gmail.com>
2019-10-07 22:30:27 +0800

    export proble detail

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -1,5 +1,6 @@
 import {
     Problems,
+    ProblemDetail,
     Status,
     Home,
     About
@@ -15,9 +16,10 @@ const routes = [
 
     // problem
     {path: '/problems', component: Problems},
-    {path: '/problem/:id/detail', name: 'problem detail'},
+    {path: '/problem/:id/detail', name: 'problem_detail', component: ProblemDetail},
     {path: '/status', component: Status},
     {path: '/about', component: About},
+
   ]
 const router = new VueRouter({
     routes,
