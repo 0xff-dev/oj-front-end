984f42474800a79165773510f371a66e734883c4
hulunbao <hulunbao@gmail.com>
2019-10-11 10:07:49 +0800

    导入contests页面

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -2,7 +2,8 @@ import Problems from './problems.vue'
 import Status from './status.vue'
 import Home from './home.vue'
 import About from './about.vue'
+import Contests from './contests.vue'
 
 export {
-    Problems, Status, Home, About
+    Problems, Status, Home, About,Contests
 }
