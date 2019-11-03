f880642a5fdbfc2b6229107480dbe8c92ffc5a39
shuang zhang <stevenshuang521@gmail.com>
2019-11-03 23:32:21 +0800

    beta token

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,8 @@ export default new Vuex.Store({
     state: {
         tabIndex: 0,
         panelStatus: 0, // 0不显示，1显示login，2显示register
+        token: localStorage.getItem("user_token") || '',
+        userinfo: {}
     },
     mutations: {
         // 对于head的跳转
@@ -16,6 +18,12 @@ export default new Vuex.Store({
         // 登录，注册面板
         setPanelStatus(state, status) {
             state.panelStatus = status
-        }
+        },
+        setUserToken(state, val) {
+            state.token = val
+        },
+        setUserInfo(state, val) {
+            state.userinfo = val
+        } 
     }
 });
